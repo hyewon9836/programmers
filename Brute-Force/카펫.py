bf8290ddee30b32adfdee3ec8cd2a84093b672f4
hyewon9836 <74521008+hyewon9836@users.noreply.github.com>
2021-11-10 11:20:35 +0900

    Add files via upload

---
+++ Brute-Force/카펫.py
@@ -0,0 +1,7 @@
+def solution(brown,yellow):
+    total = brown+yellow
+    for i in range(total,2,-1):
+        if total%i==0:
+            j=total//i
+            if yellow==(i-2)*(j-2):
+                return [i,j]
