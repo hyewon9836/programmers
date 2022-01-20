5bfe93b74e2890519ddb9d9965c57de77ce6d40d
hyewon9836 <74521008+hyewon9836@users.noreply.github.com>
2022-01-20 17:25:28 +0900

    Add files via upload

---
+++ Kakao Blind Recruitment/level3/추석 트래픽.py
@@ -0,0 +1,17 @@
+import datetime
+def solution(lines):
+    dateformat="%Y-%m-%d %H:%M:%S.%f"
+    answer=0
+    end=[];start=[]
+    for i,time in enumerate(lines):
+        endtime=time[:23]
+        end.append(datetime.datetime.strptime(endtime,dateformat))
+        gap=(float)(time[23:-1])
+        start.append(end[i]-datetime.timedelta(seconds=gap)+datetime.timedelta(seconds=0.001))
+    for i in range(len(lines)):
+        cnt=0
+        for j in range(i,len(lines)):
+            if start[j]-end[i]<datetime.timedelta(seconds=1):
+                cnt+=1
+        answer=max(answer,cnt)
+    return answer
